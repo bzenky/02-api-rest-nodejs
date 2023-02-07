7fd57e3edbe8f9e77dec770d914d2e8c32d1cfc3
Zenky <bzenky@gmail.com>
2023-02-07 20:18:05 -0300

    feat: queries with knex

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,23 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async (req, res) => {
-  const tables = await knex('sqlite_schema').select('*')
+  // const transaction = await knex('transactions')
+  //   .insert({
+  //     id: crypto.randomUUID(),
+  //     title: 'Transação de teste',
+  //     amount: '1000',
+  //   })
+  //   .returning('*')
 
-  return tables
+  const transaction = await knex('transactions')
+    .select('*')
+    .where('amount', 10000)
+
+  return transaction
 })
 
 app
