0b7cc372bbf51f18f5e47fe6849e208cbace2432
Zenky <bzenky@gmail.com>
2023-02-07 21:17:57 -0300

    feat: env validation with zod

--- src/database.ts
+++ src/database.ts
@@ -1,5 +1,5 @@
-import 'dotenv/config'
 import { knex as setupKnex, Knex } from 'knex'
+import { env } from './env'
 
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL not found.')
@@ -8,7 +8,7 @@ if (!process.env.DATABASE_URL) {
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: process.env.DATABASE_URL,
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
