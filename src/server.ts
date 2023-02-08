66fd7cde1fe6fd1513f3f8d7b86597ca4dff69fe
Zenky <bzenky@gmail.com>
2023-02-07 21:03:48 -0300

    feat: env

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,9 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async (req, res) => {
-  // const transaction = await knex('transactions')
-  //   .insert({
-  //     id: crypto.randomUUID(),
-  //     title: 'Transação de teste',
-  //     amount: '1000',
-  //   })
-  //   .returning('*')
-
   const transaction = await knex('transactions')
     .select('*')
     .where('amount', 10000)
