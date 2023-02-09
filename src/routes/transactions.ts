13e75ab5c50120e966fe507b89af5f6bb91e6a6c
Zenky <bzenky@gmail.com>
2023-02-09 19:49:54 -0300

    feat: fastify cookies

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -47,10 +47,22 @@ export async function transactionsRoutes(app: FastifyInstance) {
       request.body,
     )
 
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // miliseconds - 7 days
+      })
+    }
+
     await knex('transactions').insert({
       id: randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
     })
 
     return reply.status(201).send()
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
 import { env } from './env'
 import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
+app.register(cookie)
 app.register(transactionsRoutes, {
   prefix: 'transactions',
 })
